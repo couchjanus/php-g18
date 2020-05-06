b4fde658a8651342fbd59f0c6301a76c4a61bbcf
couch Janus <couchjanus@gmail.com>
2020-05-06 14:25:24 +0300

    added unit 3

---
+++ logs/error_log.php
@@ -0,0 +1,4 @@
+[06-May-2020 14:09:41 Europe/Kiev] Hello Log!
+[06-May-2020 14:17:56 Europe/Kiev] Hello Log!
+[06-May-2020 14:17:56 Europe/Kiev] PHP Warning:  require_once(/home/janus/www/php-g18/app/Views/layouts/partials/_head.php): failed to open stream: No such file or directory in /home/janus/www/php-g18/app/Views/layouts/app.php on line 3
+[06-May-2020 14:17:56 Europe/Kiev] PHP Fatal error:  require_once(): Failed opening required '/home/janus/www/php-g18/app/Views/layouts/partials/_head.php' (include_path='.:/usr/share/php') in /home/janus/www/php-g18/app/Views/layouts/app.php on line 3
--- public/index.php
+++ public/index.php
@@ -1,83 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <ul>
-        <li><a href="get.php">Get</a></li>
-        <li><a href="post.php">Post</a></li>
-        <li><a href="req.php">Request</a></li>
-    </ul>
-    <?php
+<?php
 
-    function foo($a, $b = "Hello ")
-    {
-        echo "<h2>Inside function.</h2>";
-        $retval = "<h2>".$b.$a."</h2>"; 
-        return $retval;
-    }
+// Подключаем файл, где храниться автозагрузчик
 
-    echo foo("World");
+// var_dump(__DIR__);
+// define('ROOT', dirname(__DIR__));
+// var_dump(ROOT);
 
-    $a = "<h3>глобальная область видимости</h3>"; /* глобальная область видимости */
+// var_dump(__DIR__ . "/../bootstrap/app.php");
+// var_dump(realpath(__DIR__ . "/../bootstrap/app.php"));
 
-    function test()    {
-        $a = "<h3>локальная область видимости</h3>";
-        echo $a; /* ссылка на переменную в локальной области видимости */
-    }
-    
-    echo $a;
-    test();
-
-    // Использование global
-    $a = 1;
-    $b = 2;
-
-    function Sum() {
-        global $a, $b;
-        $b = $a + $b;
-    } 
-    Sum();
-    echo $b;
-
-    // Использование $GLOBALS вместо global
-    //  function Sum()
-    //  {
-    //      $GLOBALS['b'] = $GLOBALS['a'] + $GLOBALS['b'];
-    //  }
-
-    // Суперглобальные переменные
-    // var_dump($GLOBALS);
-
-
-    /**
-     *  @return string текущий адрес запроса 
-     **/ 
-    function getURI()
-    {
-        if (isset($_SERVER['REQUEST_URI']) and !empty($_SERVER['REQUEST_URI'])) {
-            return trim($_SERVER['REQUEST_URI'], '/');
-        }
-    }
-    // $uri = getURI();
-    // switch ($uri) {
-    //     case '':
-    //         include 'home.php';
-    //         break; 
-    //     case 'about':
-    //         include 'about.php';
-    //         break; 
-    //     case 'contact':
-    //         include 'contact.php';
-    //         break; 
-    //     default:
-    //         include '404.php';
-    // }  
-    
-    ?>
-    </body>
-</html>
+require_once realpath(__DIR__ . "/../bootstrap/app.php");
